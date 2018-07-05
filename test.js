3b5549b01cfb4bf485910b253aaf2ec212ed564c
Jonathan Kolman <jonathankolman@Jonathans-MacBook-Pro.local>
2018-07-05 19:32:45 -0400

    successfully converting csvs to json

--- test.js
+++ test.js
@@ -1,53 +1,33 @@
+import * as C from './config'
 import { expect } from 'chai'
-import agent from 'superagent-bluebird-promise'
-import mongoose from 'mongoose'
-import models from './models'
+import fs from 'fs'
+import csv from 'csvtojson'
 
 
 describe('tests', () => {
-	it('should make an superagent request', done => {
-		agent.get('https://google.com')
-		.then(res => {
-			expect(res).to.be.ok
+	it('should convert mst30csvs to json', done => {
+		let csvData = {}
+		let csvFilePath = './mst30csvs'
+		const readDir = Promise.promisify(require('fs').readdir)
+		readDir(csvFilePath)
+		.each(file => {
+			csvData[file] = []
+			return csv().fromFile(csvFilePath+'/'+file).subscribe(json => csvData[file].push(json), (err) => Promise.reject(err), () => Promise.resolve())
+		})
+		.then(() => {
+			//keys corresponds to the name of the csv file
+			let keys = Object.keys(csvData)
+			expect(keys).to.be.an.array
+			keys.forEach(k => {
+				//json for file
+				let data = csvData[k]
+				console.log(`data for file ${k}`, data)
+				console.log(`--------------end of data for ${k}--------------`)
+			})
+
 			done()
 		})
+
 	})
-	it('should load environment variables from .env file', done => {
-		require('dotenv').load()
-		expect(process.env.DATABASE_URL).to.not.be.undefined
-		expect(process.env.DATABASE_URL).to.eq('mongodb://localhost/node-starter-project')
-		done()
-	})
-	it('should initialize bullshit database', done => {
-		mongoose.connect(process.env.DATABASE_URL).then(connection => {
-			expect(connection).to.not.be.undefined
-			done()
-		})
-	})
-	it('should remove bullshit from mongodb', done => {
-		mongoose.model('bullshit').remove().then(res => {
-			expect(res).to.not.be.undefined
-			done()
-		})
-	})
-	it('should write some bullshit in mongodb', done => {
-		let bullshit = {
-			name:'jon kolman',
-			profession:'full stack developer',
-			age: '23'
-		}
-		mongoose.model('bullshit').create(bullshit).then(doc => {
-			expect(doc).to.not.be.undefined
-			doc.save()
-			done()
-		})
-	})
-	it('should get bullshit', done => {
-		mongoose.model('bullshit').find().count()
-		.then(count => {
-			console.log('found', count)
-			expect(count).to.eq(1)
-			done()
-		})
-	})
+	
 })
